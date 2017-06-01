8dfcebb4a69b79e87c565bb1374dd1841cf4a0ae
Peter Siska <pesche@gridonic.ch>
2017-06-01 11:56:07 +0200

    Add font conversion

--- src/AsciiFun/AsciiFun.js
+++ src/AsciiFun/AsciiFun.js
@@ -1,17 +1,25 @@
 import Vue from 'vue';
 import figlet from 'figlet';
 
+import fontFilenames from './fonts.json';
+
+const fonts = fontFilenames.map(x => x.slice(0, x.length - 4));
+
 export default Vue.extend({
     name: 'AsciiFun',
     data() {
         return {
             term: 'Start Typing',
-            asciiArt: null
+            asciiArt: null,
+            fonts,
+            selectedFont: fonts[Math.floor(Math.random() * fonts.length)]
         }
     },
     methods: {
         asciize: function() {
-            figlet(this.term, (err, data) => {
+            figlet(this.term, {
+                font: this.selectedFont
+            }, (err, data) => {
                 if (err) {
                     console.error(err);
                     this.asciiArt = 'Error…';
