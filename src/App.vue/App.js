21ef11780344f1eb48057c954a499107a9bbb4d1
Dennis Schenk <dennis@gridonic.ch>
2017-06-01 21:53:14 +0200

    Make it more oldschool!

--- src/App.vue/App.js
+++ src/App.vue/App.js
@@ -3,6 +3,6 @@ import Vue from 'vue';
 export default Vue.extend({
     components: {
         // @see https://vuejs.org/v2/guide/components.html#Async-Components
-        Asciiizer: () => System.import('../Asciiizer')
+        Asciiizer: () => System.import('../Asciiizer.vue')
     }
 });
